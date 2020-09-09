fcf483b1615d67f6221aa5567a47a44f7dd89191
codwerk <louis_widdy@live.fr>
2020-09-09 23:03:18 +0200

    first commit: install vue-ressource, pug, sass

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #app
+    show-blogs
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import showBlogs from './components/showBlogs.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'show-blogs': showBlogs
   }
 }
 </script>
---
+++ src/components/showBlogs.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  #show-blogs
+    h1 All Blog Articles
+
+    .single-blog(v-for="")
+
+</template>
+
+<script>
+ export default {
+   data: () => {
+     return {}
+   },
+   methods: {
+
+   },
+   created() {
+
+   }
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
